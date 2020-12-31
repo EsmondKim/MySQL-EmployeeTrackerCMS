06329650bc1b5d95a0b8440cc6e4121c19e33be7
EsmondKim <ekelectronicmail@gmail.com>
2020-12-31 17:49:56 -0600

    Started seed.sql file.

--- db/schema.sql
+++ db/schema.sql
@@ -4,21 +4,37 @@ CREATE database employeesdb;
 USE employeesdb;
 
 CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT,
-    manager_id INT
+    salary DECIMAL,
+    FOREIGN KEY (roles_id) REFERENCES roles (id),
+    FOREIGN KEY (employee_id) REFERENCES employee (id),
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE roles (
+  id INT NOT NULL AUTO_INCREMENT,
   title VARCHAR(30),
   salary DECIMAL,
-  department_id INT
+  department_id INT,
+  PRIMARY KEY (id)
 );
 
 CREATE TABLE department (
-  
+  id INT NOT NULL AUTO_INCREMENT,
+  dept_name VARCHAR(30) NOT NULL,
+  utilized_budget DECIMAL,
+  PRIMARY KEY (id)
 );
 
-SELECT * FROM top5000;
-select * from top_albums
+CREATE TABLE manager (
+  id INT NOT NULL AUTO_INCREMENT,
+  mgr_name VARCHAR(30),
+  PRIMARY KEY (id)
+)
+
+SELECT * FROM employee;
+SELECT * FROM roles;
+SELECT * FROM department;
+SELECT * FROM manager;
---
+++ db/seed.sql
@@ -0,0 +1,2 @@
+INSERT INTO employee (first_name, last_name, salary, role_id, manager_id, mgr_name)
+VALUES ('Anthony', 'Soprano', 250000, )
