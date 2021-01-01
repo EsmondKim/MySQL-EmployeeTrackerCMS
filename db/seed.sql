d02ff5f948aaf7af8aff2f2effe97aef7bd2fc91
EsmondKim <ekelectronicmail@gmail.com>
2020-12-31 18:59:27 -0600

    Modified seed.sql to work correctly in MySQL Workbench.

--- db/seed.sql
+++ db/seed.sql
@@ -1,20 +1,8 @@
-INSERT INTO employee (first_name, nickname, last_name, salary, manager_id, roles_id)
-VALUES ('Anthony', 'The Boss', 'Soprano', 1000000, null, 1),
-       ('Jennifer', 'Melfi', 75000, 1, 2),
-       ('Christopher', 'The Nephew', 'Moltisanti', 100000, null, 3),
-       ('Peter', 'Paulie Walnuts', 'Gualtieri', 100000, 3, 3),
-       ('Patsy', 'Parisi', 100000, 3, 3),
-       ('Furio', 'The Enforcer', 'Giunta', 100000, null, 4),
-       ('Anthony', 'Lil Tony','Blundetto', 100000, 6, 4),
-       ('Vito', 'Spatafore', 100000, 6, 4),
-       ('Ralph', 'Ralphie', 'Cifaretto', 200000, 1, 6),
-       ('Arthur', 'Artie the Chef', 'Bucco', 50000, 1, 5);
-
 INSERT INTO department (dept_name, utilized_budget)
 VALUES ('Administration', 1000000),
        ('Therapy and Human Resources', 75000),
        ('Debt Collection', 300000),
-       ('Chiropractic, Surgerical, and Firearms Unit', 300000),
+       ('Chiropractic and Firearms Unit', 300000),
        ('Cafeteria and Catering', 50000),
        ('Credit and Lending', 400000);
        
@@ -25,3 +13,15 @@ VALUES ('Boss of Bosses', 1000000),
        ('Negotiator', 150000),
        ('Chef', 50000),
        ('Loan Broker', 200000);
+	
+INSERT INTO employee (first_name, nickname, last_name, salary, manager_id, roles_id)
+VALUES ('Anthony', 'The Boss', 'Soprano', 1000000, null, 1),
+       ('Jennifer', Null, 'Melfi', 75000, 1, 2),
+       ('Christopher', 'The Nephew', 'Moltisanti', 100000, null, 3),
+       ('Peter', 'Paulie Walnuts', 'Gualtieri', 100000, 3, 3),
+       ('Patsy', Null, 'Parisi', 100000, 3, 3),
+       ('Furio', 'The Enforcer', 'Giunta', 100000, null, 4),
+       ('Anthony', 'Lil Tony','Blundetto', 100000, 6, 4),
+       ('Vito', Null, 'Spatafore', 100000, 6, 4),
+       ('Ralph', 'Ralphie', 'Cifaretto', 200000, 1, 6),
+       ('Arthur', 'Artie the Chef', 'Bucco', 50000, 1, 5);
