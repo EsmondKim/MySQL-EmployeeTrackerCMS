2c3c040936a6acf14fcecf7d3f89b16cea59c9da
EsmondKim <ekelectronicmail@gmail.com>
2020-12-31 18:54:26 -0600

    Modified schema.sql to work in MySQL Workbench.

--- db/schema.sql
+++ db/schema.sql
@@ -3,25 +3,6 @@ CREATE database employeesdb;
 
 USE employeesdb;
 
-CREATE TABLE employee (
-    id INT NOT NULL AUTO_INCREMENT,
-    first_name VARCHAR(30) NOT NULL,
-    nickname VARCHAR(30),
-    last_name VARCHAR(30) NOT NULL,
-    salary DECIMAL,
-    FOREIGN KEY (roles_id) REFERENCES roles (id),
-    FOREIGN KEY (manager_id) REFERENCES employee (id),
-    PRIMARY KEY (id)
-);
-
-CREATE TABLE roles (
-  id INT NOT NULL AUTO_INCREMENT,
-  title VARCHAR(30),
-  salary DECIMAL,
-  FOREIGN KEY (department_id) REFERENCES department (id),
-  PRIMARY KEY (id)
-);
-
 CREATE TABLE department (
   id INT NOT NULL AUTO_INCREMENT,
   dept_name VARCHAR(30) NOT NULL,
@@ -29,7 +10,29 @@ CREATE TABLE department (
   PRIMARY KEY (id)
 );
 
+CREATE TABLE roles (
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(30),
+  salary DECIMAL,
+  department_id INT,
+  FOREIGN KEY (department_id) REFERENCES department (id),
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    nickname VARCHAR(30),
+    last_name VARCHAR(30) NOT NULL,
+    salary DECIMAL,
+    roles_id INT,
+    manager_id INT,
+    FOREIGN KEY (roles_id) REFERENCES roles (id),
+    FOREIGN KEY (manager_id) REFERENCES employee (id),
+    PRIMARY KEY (id)
+);
+
 SELECT * FROM employee;
 SELECT * FROM roles;
 SELECT * FROM department;
-SELECT * FROM manager;
+
