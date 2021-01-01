4344e94cee1f3bc02382304254362438653de995
EsmondKim <ekelectronicmail@gmail.com>
2020-12-31 18:23:03 -0600

    Added department budgets.

--- db/seed.sql
+++ db/seed.sql
@@ -13,10 +13,10 @@ VALUES ('Anthony', 'The Boss', 'Soprano', 1000000),
 INSERT INTO department (dept_name, utilized_budget)
 VALUES ('Administration', 1000000),
        ('Therapy and Human Resources', 75000),
-       ('Debt Collection',),
-       ('Chiropractic, Surgerical, and Firearms Unit'),
-       ('Cafeteria and Catering'),
-       ('Credit and Lending');
+       ('Debt Collection', 500000),
+       ('Chiropractic, Surgerical, and Firearms Unit', 300000),
+       ('Cafeteria and Catering', 50000),
+       ('Credit and Lending', 400,000);
        
 INSERT INTO roles (title, salary)
 VALUES (),
