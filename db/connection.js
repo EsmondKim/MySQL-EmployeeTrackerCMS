b856092173ab9688d5c05ab47ebc1461e5ecc355
EsmondKim <ekelectronicmail@gmail.com>
2021-01-01 15:24:01 -0600

    Added connection to connections.js and started index.js.

--- db/connection.js
+++ db/connection.js
@@ -4,19 +4,17 @@ let inquirer = require("inquirer");
 let connection = mysql.createConnection({
   host: "localhost",
 
-  // Your port; if not 3306
   port: 3306,
 
-  // Your username
   user: "root",
 
-  // Your password
   password: "vegetable",
-  database: "employees_seed"
+  database: "employeesdb"
 });
 
 connection.connect(function(err) {
   if (err) throw err;
-  runSearch();
+    console.log("connected as id " + connection.threadId + "\n");
+  //runSearch();
 });
 
